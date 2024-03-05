9bb8302ed052bc2fe1dc89c8a537e5c200958567
simoneevacca <v.simone.vacca@gmail.com>
2024-03-05 14:36:42 +0100

    snack-3 done

--- snack-3/main.js
+++ snack-3/main.js
@@ -2,3 +2,19 @@
 //Ogni animale ha un nome, una famiglia e una classe.
 //Crea un nuovo array con la lista dei mammiferi.
 
+const animals = [
+    { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+    { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+    { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+]
+
+
+const mammiferi = animals.filter(animal => animal.classe === 'mammiferi');
+//log in console la stringa con tutte le informazioni disponibili solo dei mammiferi
+console.log(mammiferi);
+
+mammiferi.forEach((element) => {
+
+    // log in console solo i nomi dei mammiferi
+    console.log(element.nome);
+})
